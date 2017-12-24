ce9a400dcaede68057e9313c2794c45ffa0416f3
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-24 01:50:06 +0100

    Creation projet Maven

---
+++ Go/src/main/java/ensim/Go/App.java
@@ -0,0 +1,13 @@
+package ensim.Go;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
