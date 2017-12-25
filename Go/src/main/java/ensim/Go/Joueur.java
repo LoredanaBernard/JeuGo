0e9d61469a3b35c5134e3f51b0379181d48ebed6
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-25 23:55:16 +0100

    Creation classe joueur

---
+++ Go/src/main/java/ensim/Go/Joueur.java
@@ -0,0 +1,29 @@
+package ensim.Go;
+
+public class Joueur {
+	Goban goban;
+	String couleur;
+	int nPierre = 1;
+	
+	Joueur(String c){
+		this.couleur=c;
+	}
+	
+	// Place une pierre sur le goban
+	public void placerPierre(Goban goban, int x, int y) {
+		if (goban.estLibre(x, y)) {		// Si l'intersecrion du goban n'e'st pas encore prise
+			Pierre p = new Pierre(this.couleur + this.nPierre); // Création d'une nouvelle pierre
+			p.x = x;
+			p.y = y;
+			goban.matricePierre[x][y]= p;	// Attribution de la pierre à une intersection du goban
+			nPierre++;
+		}		
+	}
+	
+	// Capture pierre(s) adverse(s) 
+	public void capturePierre(Goban goban, int x, int y) {
+		
+		// Capture les pierres adverses qui n'ont plus de libertés 
+	}
+
+}
