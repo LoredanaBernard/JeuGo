8718d2dfa1d1b7f996f470f771e80e9523bd6923
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-27 22:35:10 +0100

    Creation des tests classe Goban

---
+++ Go/src/test/java/ensim/Go/GobanTest.java
@@ -0,0 +1,36 @@
+package ensim.Go;
+
+import junit.framework.TestCase;
+
+public class GobanTest extends TestCase {
+	Goban goban;
+
+	public void testAfficherGoban() {
+		/*goban = new Goban();
+		String af = new String();
+		for ( int i=0 ; i<9 ; i++) {
+			for ( int j=0 ; j<9 ; j++) {
+				af += " + ";
+			}
+		}
+		assertEquals(af,goban.afficherGoban());*/
+	}
+
+	public void testEstLibre() {
+		goban = new Goban();
+		Pierre p = new Pierre ("b1","n");
+		goban.matricePierre[1][1] = p;
+		assertEquals(false,goban.estLibre(1, 1));
+		assertEquals(true,goban.estLibre(0, 1));
+	}
+
+	public void testRetourPierre() {
+		goban = new Goban();
+		assertEquals(null,goban.retourPierre(0, 2));
+		
+		Pierre p = new Pierre ("b1","n");
+		goban.matricePierre[1][1] = p;
+		assertEquals(p,goban.retourPierre(1, 1));
+	}
+
+}
