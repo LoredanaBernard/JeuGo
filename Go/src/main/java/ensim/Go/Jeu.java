79840d3317988efc234ae62d5538d876605287cd
Unknown <loredana.bernard.etu@univ-lemans.fr>
2017-12-29 02:23:51 +0100

    Allégement classe Jeu

--- Go/src/main/java/ensim/Go/Jeu.java
+++ Go/src/main/java/ensim/Go/Jeu.java
@@ -5,7 +5,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Jeu {
+	static Scanner sc; 
+	static int l;
+	static int c;
+	static Goban goban;
 	
+	public static int initialiserJeu() {
+		int t;
+		sc = new Scanner(System.in);
+		System.out.print("Taille du goban : ");
+		int str = sc.nextInt();
+		t = str;
+		if ( t<0) {
+			initialiserJeu();
+		}
+		return t;
+	}
+	
+	// Affichage du gagnant et des scores
 	public static void finDeJeu (Joueur j1, Joueur j2) {
 		int scoreJ1 = j1.nPierre -j2.nCaptureAdverse;
 		int scoreJ2 = j2.nPierre -j1.nCaptureAdverse;
@@ -25,63 +42,43 @@ public class Jeu {
 		}
 		
 	}
+	
+	// Saisie des coordonnes de pierres à placer et capture si possible
+	public static void afficherInstructions(Joueur j, String nom, Goban goban) {
+		sc = new Scanner(System.in);
+		int x1= -1;
+		while ( x1<0 || x1>8 ) {
+			System.out.println(nom + ", veuillez saisir une position de pierre :");
+			System.out.print("Ligne : ");
+			int str = sc.nextInt();
+			x1 = str;
+		}
+		l=x1;
+		int y2=-1;
+		while ( y2<0 || y2>8 ) {
+			System.out.print("Colonne : ");
+			int str3 = sc.nextInt();
+			y2 = str3;
+		}
+		c =y2;
+		Pierre p = j.placerPierre(goban,l, c);
+		goban.afficherGoban();
+		j.capturePierre(goban,p);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
 		
-		Goban goban = new Goban();
-		Joueur j1 = new Joueur("n");
-		Joueur j2 = new Joueur ("b");
-		Scanner sc = new Scanner(System.in);
-		int x;
-		int y;
+		Goban goban = new Goban(initialiserJeu());
+		Joueur j1 = new Joueur("b");
+		Joueur j2 = new Joueur("n");
+		
 		do {
-			// Joueur 1
-			int x1= -1;
-			while ( x1<0 || x1>8 ) {
-				System.out.println("Joueur 1 :veuillez saisir une position de pierre :");
-				System.out.print("x : ");
-				int str = sc.nextInt();
-				x1 = str;
-			}
-			x=x1;
-			int y2=-1;
-			while ( y2<0 || y2>8 ) {
-				System.out.print("y : ");
-				int str3 = sc.nextInt();
-				y2 = str3;
-			}
-			y =y2;
-			Pierre p = j1.placerPierre(goban,x, y);
-			goban.afficherGoban();
-			j1.capturePierre(goban,p);
-			
-			// Joueur 2
-			int x3=-1;
-			while( x3<0 || x3>8 ) {
-				System.out.println(" Joueur 2 : veuillez saisir une position de pierre :");
-				System.out.print("x : ");
-				int str2 = sc.nextInt();
-				x3 = str2;
-			}
-			x=x3;
-			int y3=-1;
-			while ( y3<0 || y3>8 ) {
-				System.out.print("y : ");
-				int str2 = sc.nextInt();
-				y3 = str2;
-			}
-			y=y3;
-			
-			Pierre d = j2.placerPierre(goban,x, y);
-			goban.afficherGoban();
-			j2.capturePierre(goban,d);
-			
+			afficherInstructions(j1, "Joueur 1", goban);
+			afficherInstructions(j2, "Joueur 2", goban);					
 			System.out.print("Continuer ? ( -1 pour finir) ");
 		}while ( sc.nextInt() != -1);
-		
-		
+				
 		finDeJeu(j1,j2);
 
 }
